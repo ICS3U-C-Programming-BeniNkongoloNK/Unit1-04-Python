72c28a0496aa4f16b0c04dd7848708f750ce6c60
benink <117493347+benink@users.noreply.github.com>
2025-02-18 17:59:48 +0000

    added basic math fuctions

---
+++ basic_math.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# Created By: Beni
+# Date: Febuary 18, 2025
+# Does basic math.
+
+
+def main():
+    print("6-4={}".format(6 - 4))
+    print("8+4={}".format(8 + 4))
+    print("2*3={}".format(2 * 3))
+    print("10/5={}".format(10 / 5))
+    print("1/2={}".format(1 / 2))
+    print("11+4*5={}".format(11 + 4 * 5))
+    print("2^3={}".format(2 ^ 3))  # bitwise operator
+    print("2**3={}".format(2**3))  # exponent
+    print("16**0.5={}".format(16**0.5))  # square root
+
+
+if __name__ == "__main__":
+    main()
